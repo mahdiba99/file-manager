66e46166d432827d6111fc42b614eb679c215fd9
mahdiba99 <35558389+mahdiba99@users.noreply.github.com>
2018-04-15 13:03:55 +0430

    new item

--- Item.py
+++ Item.py
@@ -1,7 +1,13 @@
+import os
 class Item:
-    def __init__(self,name,size,date,path):
+    def __init__(self,name = None,size = None,date = None,path =None):
         self.name = name
         self.size = size
         self.path = path
         self.date = date
-
+        self.set_icon()
+    def set_icon(self):
+        if os.path.isdir(self.path + "/" + self.name):
+            self.icon = 'folder'
+        else:
+            self.icon = 'file'
